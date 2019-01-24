2f2272e651907c4eddc895b797f94881effc3a2e
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2019-01-24 11:09:16 +0100

    KATA 24/01/2019 :lipstick: :sparkles: Display users, 100% front

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,8 +1,17 @@
 <template>
   <div id='app'>
     <div class='initial-message white'>{{ firstMessage }}</div>
-    <div class='initial-message blue'>{{ secondMessage }}</div>
-    <div class='initial-message white'>{{ thirdMessage }}</div>
+    <div class='guess-who'>Kiki {{ selectedUser.firstName }}?</div>
+    <div class='users-list-container'>
+      <div
+        v-for='user in users'
+        class='user-container'
+        @click='checkAnswer(user)'
+      >
+        <img class="mug-shot" :src="`photos/${user.photo}`">
+        </img>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,16 +20,42 @@
     data: function () {
       return {
         firstMessage: "Skello",
-        secondMessage: "KATA",
-        thirdMessage: "vuejs App!",
+        users: [],
+        selectedUser: {firstName: '', photo: ''},
       }
+    },
+    mounted() {
+      this.fetchUsers();
+    },
+    methods: {
+      fetchUsers() {
+        this.users = [
+          { firstName: 'Bart', photo: 'bart.jpeg' },
+          { firstName: 'Celestin', photo: 'celo.jpeg' },
+          { firstName: 'Agathe', photo: 'agathe.jpeg' },
+          { firstName: 'Victor', photo: 'victor_l.jpeg' },
+          { firstName: 'Benoit', photo: 'benoit.jpeg' },
+          { firstName: 'Hien-Chanh', photo: 'HC.jpeg' },
+          { firstName: 'Bart', photo: 'bart.jpeg' },
+          { firstName: 'Alex', photo: 'alex.jpeg' },
+          { firstName: 'Ari', photo: 'ari.jpeg' },
+          { firstName: 'Tristan', photo: 'dentiste.jpeg' },
+          { firstName: 'Samy', photo: 'samy.jpeg' },
+          { firstName: 'Kit', photo: 'Kit.jpeg' },
+        ];
+        this.selectedUser = this.users[Math.floor ( Math.random() * this.users.length )];
+      },
+      checkAnswer() {
+
+      },
     }
   }
 </script>
 
-<style lang='scss' scoped>
+{/*<style lang='scss' scoped>
   p {
     font-size: 2em;
     text-align: center;
   }
 </style>
+*/}
