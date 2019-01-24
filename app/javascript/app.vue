8b74c33852d167629582da14c89f9660095cf631
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2019-01-24 09:53:03 +0100

    KATA :tada: vuejs initial commit

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id='app'>
+    <div class='initial-message white'>{{ firstMessage }}</div>
+    <div class='initial-message blue'>{{ secondMessage }}</div>
+    <div class='initial-message white'>{{ thirdMessage }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        firstMessage: "Skello",
+        secondMessage: "KATA",
+        thirdMessage: "vuejs App!",
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
