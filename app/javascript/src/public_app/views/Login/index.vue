5d8c7c76aa931922337a36218b2b166dba70da4f
Barthelemy Ducrot <barthelemy.ducrot@hei.fr>
2020-01-10 19:05:17 +0100

    TKTTKT :sparkles: start login view

---
+++ app/javascript/src/public_app/views/Login/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id='login'>
+  </div>
+</template>
+
+<script>
+  import {
+    users, coachBu, salesBu, recrutementBu, produitBu, grandsComptesBu,
+    techBu, sdrBu, marketingBu, associesBu, opsBu, teamLeadersBu, BUList
+  } from '../../constants.js'
+
+  export default {
+    name: 'Login',
+    data() {
+      return {}
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
